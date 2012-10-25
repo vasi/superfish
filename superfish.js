e9af00aed3e46d707f17e5d3d292496cd99afbf9
mehrpadin <mail@mehrpadin.net>
2012-10-25 23:07:51 +0330

    Issue #1347710 by mehrpadin: Fixed a bug in the Drupal Behaviors file. (Comment 13th)

--- superfish.js
+++ superfish.js
@@ -1,7 +1,9 @@
+/**
+ * @file
+ * The Superfish Drupal Behavior to apply the Superfish jQuery plugin to lists.
+ */
+
 (function ($) {
-  /**
-   * The Superfish Drupal Behavior to apply the Superfish jQuery plugin to lists.
-   */
   Drupal.behaviors.superfish = {
     attach: function (context, settings) {
       // Take a look at each list to apply Superfish to.
@@ -9,14 +11,19 @@
         // Process all Superfish lists.
         $('#superfish-' + options.id, context).once('superfish', function() {
           var list = $(this);
-          // Apply Superfish to the list.
-          list.superfish(options.sf);
 
-          // Check if we are to apply any plugins to it.
+          // Check if we are to apply the Supersubs plug-in to it.
           if (options.plugins || false) {
             if (options.plugins.supersubs || false) {
               list.supersubs(options.plugins.supersubs);
             }
+          }
+
+          // Apply Superfish to the list.
+          list.superfish(options.sf);
+
+          // Check if we are to apply any other plug-in to it.
+          if (options.plugins || false) {
             if (options.plugins.touchscreen || false) {
               list.sftouchscreen();
             }
