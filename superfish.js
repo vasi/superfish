c7d38740dd0313ccc1f8e39f9f0ad4bf5fcdbd99
mehrpadin <mail@mehrpadin.net>
2013-02-08 15:05:19 +0330

    Added a missed piece.

--- superfish.js
+++ superfish.js
@@ -25,7 +25,7 @@
           // Check if we are to apply any other plug-in to it.
           if (options.plugins || false) {
             if (options.plugins.touchscreen || false) {
-              list.sftouchscreen();
+              list.sftouchscreen(options.plugins.touchscreen);
             }
             if (options.plugins.smallscreen || false) {
               list.sfsmallscreen(options.plugins.smallscreen);
